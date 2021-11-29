8860c1045507041dbdb92b1eb3490607ee7642dc
clyntriley <91975117+clyntriley@users.noreply.github.com>
2021-11-29 18:08:16 +0200

    Update calculator_history.dart

--- lib/calculator_history.dart
+++ lib/calculator_history.dart
@@ -1,36 +1,67 @@
 import 'package:flutter/material.dart';
-import 'calculator_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 
 
 
 class MyHistoryApp extends StatelessWidget {
-  const MyHistoryApp({Key? key, required this.text}) : super(key: key);
-  final String text;
+  MyHistoryApp({Key? key ,required this.text}) : super(key: key);
+
+    String text;
+
+  void initState() {
+    super.toString();
+    loadHistory();
+  }
+
+   loadHistory() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.getString('history')! ;
+      
+  }
+   saveHistory() async {
+    final prefs = await SharedPreferences.getInstance();
+      prefs.setString('history', text);
+  
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
     DateTime now = DateTime.now();
     return Scaffold(
-      appBar: AppBar(title: const Text('Third screen')),
-      body: Center(
-        child: Column(
-          children: <Widget> [
-            Text(
-              'Equation: $text',
-              style: const TextStyle(fontSize: 15),
-            ),
-            Center(
-              child: Text('timestamp: $now',
-                  style: const TextStyle(fontSize: 15)),
-            )
-          ]
-
-        )
-
+      appBar: AppBar(
+        title: const Text("History page"),
       ),
+    body: Column(
+      children: [
+        Text('Equation: $text',style: const TextStyle(fontSize: 15)),
+
+        ElevatedButton(
+          onPressed: () {
+            loadHistory();
+            saveHistory();
+          },
+          child: const Text('Save'),
+        ),
+
+        Center(
+          child: Text('timestamp: $now',
+              style: const TextStyle(fontSize: 15)),
+        )
+      ],
+    ),
     );
   }
 }
+
+
+
+
+
+
+
+
 
 
