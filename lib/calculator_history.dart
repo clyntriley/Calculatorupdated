3ccf17f6d1686aa93602e4c262e8f824bf547056
clyntriley <91975117+clyntriley@users.noreply.github.com>
2021-12-01 12:15:12 +0200

    Update calculator_history.dart

--- lib/calculator_history.dart
+++ lib/calculator_history.dart
@@ -1,251 +1,69 @@
 import 'package:flutter/material.dart';
-import 'package:math_expressions/math_expressions.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import "calculator_page.dart";
 
 
-import 'calculator_history.dart';
-import 'converter_page.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyHistoryApp extends StatefulWidget {
+  const MyHistoryApp({Key? key}) : super(key: key);
+   static String text = '';
 
   @override
-  Widget build(BuildContext context) {
-    return  MaterialApp(
-      home: const MyHomePage(),
-      routes: <String, WidgetBuilder>{
-        MyHistoryApp.text: (context) => const MyHistoryApp(),
-      }
-    );
-  }
+  _MyHistoryPageState createState() => _MyHistoryPageState();
 }
-TextEditingController textControllerInput = TextEditingController();
-TextEditingController textControllerResult = TextEditingController();
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+
+
+class _MyHistoryPageState extends State<MyHistoryApp> {
+  String _text = '';
+
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-Future<bool> saveNumberHistory(String number) async {
-  final prefs = await SharedPreferences.getInstance();
-  prefs.setString('history', number);
-  return prefs.commit();
-}
-Future<String> getNumberHistory() async {
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  String number = prefs.getString('history')! ;
-  return number;
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-
-  List<String> numbers = [];
-
-  addHistory() {
-    numbers.add(textControllerInput.text + ' = ' + textControllerResult.text );
-
-
+  void initState() {
+    super.initState();
+    getNumberHistory().then(updateNumber);
   }
 
-  /*void thirdScreen(BuildContext context) {
-    String textToSend = numbers.toString();
-    Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) => MyHistoryApp(text: textToSend),
-        ));
-
-
-  }*/
-
-  saveHistory() {
-    String number = numbers.toString();
-    saveNumberHistory(number).then((bool committed) {
-      Navigator.of(context).pushNamed(MyHistoryApp.text);
+  void updateNumber(String number) {
+    setState(() {
+      _text = number;
     });
   }
 
-
-
-
   @override
   Widget build(BuildContext context) {
+    DateTime now = DateTime.now();
     return Scaffold(
-      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text("History page"),
+      ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: <Widget>[
-
-
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const ConverterPage()),
-              );
-            },
-            child: const Text('Converter'),
-          ),
-          ElevatedButton(
-            child: const Icon(Icons.history, size: 30, color: Colors.black),
-            onPressed: () {
-              saveHistory();
-              //thirdScreen(context);
-            },
-          ),
-
-
-
-
-          Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: TextField(
-
-                decoration: const InputDecoration.collapsed(
-                    hintText: "0",
-                    hintStyle: TextStyle(
-                      fontSize: 30,
-                    )),
-                style: const TextStyle(
-                  fontSize: 30,
-                ),
-                textAlign: TextAlign.right,
-                controller: textControllerInput,
-              )),
-          Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: TextField(
-                decoration: const InputDecoration.collapsed(
-                    hintText: "0",
-                    // fillColor: Colors.deepPurpleAccent,
-                    hintStyle: TextStyle()),
-                textInputAction: TextInputAction.none,
-                keyboardType: TextInputType.number,
-                style: const TextStyle(
-                  fontSize: 32,
-                ),
-                textAlign: TextAlign.right,
-                controller: textControllerResult,
-              )
-          ),
-          const SizedBox( height: 15.0),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              buttonAC('AC'),
-              buttonClear(),
-              button('/',),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              button('7'),
-              button('8'),
-              button('9'),
-              button('*'),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              button('4'),
-              button('5'),
-              button('6'),
-              button('-'),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              button('1'),
-              button('2'),
-              button('3'),
-              button('+',),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              button('0'),
-              button('^'),
-              buttonEqual('='),
-            ],
-          ),
-          const SizedBox(height: 15.0,)
+        children: [
+          Text('Equation: $_text', style: const TextStyle(fontSize: 15)),
+          Center(
+            child: Text('timestamp: $now',
+                style: const TextStyle(fontSize: 15)),
+          )
         ],
       ),
     );
   }
 
-
-
-
-  Widget button(buttontext) {
-    return Container(
-      padding: const EdgeInsets.only(bottom: 10.0),
-      child: ElevatedButton(
-        child: Text(
-          buttontext,
-          style: const TextStyle(
-              fontSize: 28.0, color: Colors.black),
-        ),
-        onPressed: () {
-          setState(() {
-            textControllerInput.text = textControllerInput.text + buttontext;
-
-          });
-        },
-      ),
-    );
-  }
-
-  Widget buttonClear() {
-    return ElevatedButton(
-      child: const Icon(Icons.backspace, size: 30, color: Colors.black),
-      onPressed: () {
-        textControllerInput.text = (textControllerInput.text.isNotEmpty)
-            ? (textControllerInput.text.substring(0, textControllerInput.text.length - 1)) : "";
-      },
-    );
-  }
-  Widget buttonAC(buttonerase)  {
-    return ElevatedButton(
-      child: Text(
-        buttonerase,
-        style: const TextStyle(
-            fontSize: 20.0, color: Colors.black),
-      ),
-      onPressed: () {
-        setState(() {
-          textControllerInput.text = "";
-          textControllerResult.text = "";
-        });
-      },
-    );
-  }
-  Widget buttonEqual(buttonText) {
-    return ElevatedButton(
-      child: Text(
-        buttonText,
-        style: const TextStyle(fontSize: 20.0, color: Colors.black),
-      ),
-      onPressed: () {
-
-        addHistory();
-        Parser p = Parser();
-        ContextModel cm = ContextModel();
-        Expression exp = p.parse(textControllerInput.text);
-        setState(() {
-          textControllerResult.text = exp.evaluate(EvaluationType.REAL, cm).toString();
-        });
-      },
-    );
-  }
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
